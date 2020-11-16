d09fe1ab555ea183af3d0310e520a6bf71f2a9c0
Marcel Freiberg <marcel.freiberg@outlook.com>
2020-11-16 16:12:37 +0100

    Include imgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+#include <imgui.h>
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
 #define GLFW_INCLUDE_NONE
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -11,18 +15,49 @@ int WinMain()
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    GLFWwindow *window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "OpenGL Playground", nullptr, nullptr);
     if (!window)
     {
         glfwTerminate();
     }
 
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    gladLoadGL();
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 430");
+
     while (!glfwWindowShouldClose(window))
     {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::ShowDemoWindow();
+
+        ImGui::Render();
+
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         glfwSwapBuffers(window);
-        glfwPollEvents();
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     glfwDestroyWindow(window);
     glfwTerminate();
